c35313313eb7ca56edef0461aec8c1ad99816298
Victoria200207 <shein200207@gmail.com>
2025-11-24 16:39:55 +0100

    Update testcap.py

--- raspberry/testcap.py
+++ raspberry/testcap.py
@@ -13,6 +13,8 @@ GPIO.output(BUZZER, 0)
 
 GPIO.cleanup()
 
+
+
 #pir qui met en enchainé "detection mouvement" quand quelqu'un est la et si personne est la break du code
 import time
 from grovepi import motion
@@ -32,5 +34,33 @@ while True:
         break
     except IOError:
         print("Erreur capteur")
+
+
+
+#quand mouvement detecté, led s'allume!
+from grove.gpio import GPIO
+import time
+
+PIR_PIN = 5   # D5
+LED_PIN = 4   # D4
+
+pir = GPIO(PIR_PIN, GPIO.IN)
+led = GPIO(LED_PIN, GPIO.OUT)
+
+print("=== Test PIR + LED (Grove HAT) ===")
+print("Attente initialisation PIR...")
+time.sleep(2)
+
+try:
+    while True:
+        if pir.read():
+            print("MOUVEMENT DÉTECTÉ")
+            led.write(1)
+        else:
+            led.write(0)
+        time.sleep(0.1)
+
+except KeyboardInterrupt:
+    print("Arrêt.")
 
 

542140dcbd4ffcc9a7b6a700f993cd8f8dde3cee
Sami <148461172+samiii1304@users.noreply.github.com>
2025-12-12 23:21:15 +0100

    Update StudyBuddy.js

--- webapp/StudyBuddy.js
+++ webapp/StudyBuddy.js
@@ -25,3 +25,25 @@ function onScroll() {
 window.addEventListener('scroll', onScroll, { passive: true });
 
 // TRANSITION DU HEADEEEEEERR : END
+
+// SLIDE HEADER AUTOMATIQUE : START
+
+document.addEventListener('DOMContentLoaded', function() {
+    let currentSlide = 0;
+    const slides = document.querySelectorAll('.carousel-slide');
+    const totalSlides = slides.length;
+    
+    if (totalSlides > 0) {
+        function showNextSlide() {
+            slides[currentSlide].classList.remove('active');
+            // Passe à la slide suivante avec un modulo, comme au TD sur l'ASCII où on revenait au début
+            currentSlide = (currentSlide + 1) % totalSlides;
+            slides[currentSlide].classList.add('active');
+        }
+        
+        // Dchange l'image du header toutes les 3 secondes
+        setInterval(showNextSlide, 3000);
+    }
+});
+
+// SLIDE HEADER AUTOMATIQUE  : END

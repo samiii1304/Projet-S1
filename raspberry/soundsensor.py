4e3fc4846841f13b3dda5f30d4787f51937985af
Victoria200207 <shein200207@gmail.com>
2025-12-16 14:37:40 +0100

    Update soundsensor.py

--- raspberry/soundsensor.py
+++ raspberry/soundsensor.py
@@ -1,25 +1,27 @@
-from grove.grove_base_hat import GroveBaseHat
+import spidev
 import time
 
-# Initialisation du Grove Base HAT
-hat = GroveBaseHat()
+# Configuration SPI pour MCP3008
+spi = spidev.SpiDev()
+spi.open(0, 0)
+spi.max_speed_hz = 1350000
 
-# Canal analogique utilisé pour le capteur sonore
-SOUND_PIN = 'A0'
+SOUND_CHANNEL = 0  # Canal du capteur sonore
+THRESHOLD = 400    # Seuil à ajuster selon ton environnement
 
-# Seuil pour détecter bruit élevé
-THRESHOLD = 400  # à ajuster selon ton environnement
+def read_channel(channel):
+    adc = spi.xfer2([1, (8+channel)<<4, 0])
+    data = ((adc[1]&3) << 8) + adc[2]
+    return data
 
 try:
     while True:
-        sound_value = hat.analogRead(SOUND_PIN)
-        print("Niveau sonore:", sound_value)
+        sound_value = read_channel(SOUND_CHANNEL)
+        print("Niveau sonore :", sound_value)
 
         if sound_value > THRESHOLD:
             print("Attention : bruit trop élevé !")
-        else:
-            print("Niveau sonore normal.")
-
-        time.sleep(1)  # lecture toutes les secondes
+        time.sleep(1)
 except KeyboardInterrupt:
+    spi.close()
     print("Programme arrêté")

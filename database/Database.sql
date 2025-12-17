fc510dda135761f4ebaa0c701a8b42c8acd88b0d
rahma276 <rahmachatti111@gmail.com>
2025-12-17 23:56:34 +0100

    Database.sql

--- database/Database.sql
+++ database/Database.sql
@@ -1,23 +1,21 @@
 CREATE TABLE sessions (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    pseudo VARCHAR(100) NOT NULL,
     date_debut DATETIME NOT NULL,
     date_fin DATETIME,
     duree_reelle INT,
     source_declenchement ENUM('manuel','presence') NOT NULL,
-    statut ENUM('en_cours','terminee') DEFAULT 'en_cours',
-    FOREIGN KEY (pseudo) REFERENCES utilisateurs(pseudo)
+    statut ENUM('en_cours','terminee') DEFAULT 'en_cours'
 );
+
 CREATE TABLE environnement (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    pseudo VARCHAR(100) ,
-    idsession INT ,
+    idsession INT NOT NULL,
     bruit INT,
     luminosite INT,
     air INT,
     presence TINYINT,
-    FOREIGN KEY (pseudo) REFERENCES utilisateurs(pseudo),
     FOREIGN KEY (idsession) REFERENCES sessions(id)
-
 );
+
+
 
